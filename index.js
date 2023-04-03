429a2166b3266cddaf013a728054bea094893583
nathalia-uf <112099626+nathalia-uf@users.noreply.github.com>
2023-04-03 19:15:38 -0400

    back to original submission

--- index.js
+++ index.js
@@ -1,57 +1,54 @@
 var URL = "https://api.openweathermap.org/data/2.5/weather?q=";
-var API_KEY = "e89fac7ae6e0f6ceaf56d0c8424f5ecc";
-var weatherContainer = document.getElementById('weather');
-var form = document.querySelector('form');
+ var API_KEY = "e89fac7ae6e0f6ceaf56d0c8424f5ecc";
+ var weatherDiv = document.getElementById('weather');
+ var form = document.querySelector('form');
 
-form.onsubmit = function(e) {
-  e.preventDefault();
-  var userInput = userInput.search.value;
-  if (!userInput) return;
-  fetch(URL + searchTerm + "&units=imperial" + "&appid=" + API_KEY)
-    .then(function(res) {
-        return res.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        weatherContainer.innerHTML = "";
-        var placeName = document.createElement('h2');
-        placeName.textContent = weatherObj.place;
-        weatherContainer.appendChild(placeName);
+ form.onsubmit = function(e) {
+   e.preventDefault();
+   var searchTerm = this.search.value;
+   if (!searchTerm) return;
+   fetch(URL + searchTerm + "&units=imperial" + "&appid=" + API_KEY)
+     .then(function(res) {
+         return res.json();
+     })
+     .then(function(data) {
+         console.log(data);
+         weatherDiv.innerHTML = "";
+         var placeName = document.createElement('h2');
+         placeName.textContent = searchTerm.toUpperCase();
+         weatherDiv.appendChild(placeName);
 
-        var mapLink = document.createElement('a');
-        mapLink.href = `https://www.google.com/maps/search/?api=1&query=${data.coord.lat},${data.coord.lon}`;
-        mapLink.target = "_blank";
-        mapLink.textContent = `View on Map`;
-        weatherContainer.appendChild(mapLink);
+         var mapLink = document.createElement('a');
+         mapLink.href = `https://www.google.com/maps/search/?api=1&query=${data.coord.lat},${data.coord.lon}`;
+         mapLink.target = "_blank";
+         mapLink.textContent = `View on Map`;
+         weatherDiv.appendChild(mapLink);
 
-        var iconCode = data.weather[0].icon;
-        var description = data.weather[0].description;
+         var iconCode = data.weather[0].icon;
+         var description = data.weather[0].description;
 
-        var icon = document.createElement('img');
-        icon.src = `https://openweathermap.org/img/wn/${iconCode}.png`;
-        icon.alt = description;
-        weatherContainer.appendChild(icon);
+         var icon = document.createElement('img');
+         icon.src = `https://openweathermap.org/img/wn/${iconCode}.png`;
+         icon.alt = description;
+         weatherDiv.appendChild(icon);
 
-        var condition = document.createElement('p');
-        condition.textContent = description;
-        weatherContainer.appendChild(condition);
+         var condition = document.createElement('p');
+         condition.textContent = description;
+         weatherDiv.appendChild(condition);
 
-          addBreak()
+         var currentTemp = data.main.temp;
+         var temp = document.createElement('p');
+         temp.textContent = `Current: ${Math.round(currentTemp)}°F`;
+         weatherDiv.appendChild(temp);
 
-        var currentTemp = data.main.temp;
-        var temp = document.createElement('p');
-        temp.textContent = `Current: ${Math.round(currentTemp)}°F`;
-        weatherContainer.appendChild(temp);
+         var feelsLikeTemp = data.main.feels_like;
+         var feelsLike = document.createElement('p');
+         feelsLike.textContent = `Feels like: ${Math.round(feelsLikeTemp)}°F`;
+         weatherDiv.appendChild(feelsLike);
 
-        var feelsLikeTemp = data.main.feels_like;
-        var feelsLike = document.createElement('p');
-        feelsLike.textContent = `Feels like: ${Math.round(feelsLikeTemp)}°F`;
-        weatherContainer.appendChild(feelsLikeTemp);
-
-          addBreak()
-
-        var updatedAt = data.dt;
-        var updatedAt = document.createElement('p');
-        updatedAt.textContent = `Last updated: ${new Date(dt * 1000).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' }).toLowerCase()}`;
-        weatherContainer.appendChild(updatedAt);
-    });
+         var dt = data.dt;
+         var updated = document.createElement('p');
+         updated.textContent = `Last updated: ${new Date(dt * 1000).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' }).toLowerCase()}`;
+         weatherDiv.appendChild(updated);
+     })
+ }
